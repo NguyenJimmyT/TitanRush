4241bc5c59265b61155c7925c62b20a141180ff4
Jimmy <jimmytruc.ng@gmail.com>
2025-12-02 11:14:27 -0800

    updated estimate

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 import pandas as pd
+import numpy as np
+#from catboost import CatBoostRegressor
 import requests
 import os
 from io import StringIO
@@ -30,13 +32,32 @@ async def filter_csv():
 
     parse["datetime"] = pd.to_datetime(parse["lastUpdated"], format="mixed")
 
-    first_filter = (parse["datetime"] >= start) & (parse["datetime"] <= end_before)
-    second_filter = (parse["datetime"] >= skip) & (parse["datetime"] <= skip_end)
+    prev_sem = (parse["datetime"] >= start) & (parse["datetime"] <= end_before)
+    current_sem = (parse["datetime"] >= skip) & (parse["datetime"] <= skip_end)
 
-    filtered = parse[first_filter | second_filter]
+    filtered = parse[prev_sem | current_sem]
 
     filtered.to_csv("Spring&Fall2025.csv", index=False)
     return {"message": "filtered CSV created"}
+
+@app.get("/catModel")
+async def createModel():
+    dataset = pd.read_csv("Spring&Fall2025.csv")
+    
+    dataset['structure'] = dataset['structure'].str.title()
+    dataset['level'] = dataset['level'].str.title()
+
+    dataset = dataset.rename(columns={'timeScape': 'time', 'lastUpdated': 'date'})
+    dataset = dataset[dataset['structure'] != 'Fullerton Free Church']
+    dataset['datetime'] = dataset['datetime'].dt.tz_convert('US/Pacific')
+    dataset['time'] = dataset['date'].dt.strftime('%H:%M:%S')
+    dataset = dataset[['structure', 'level', 'available', 'total', 'date', 'time']]
+    dataset = dataset.drop_duplicates(subset=['date', 'level'])
+    dataset['available'] = dataset['available'].replace('Full', 0)
+    dataset['available'] = dataset['available'].astype(int)
+
+
+    pass
 
 @app.post("/estimate")    
 async def estimate_route(req: routing):
@@ -47,10 +68,8 @@ async def estimate_route(req: routing):
     api_req = (
         f"https://api.tomtom.com/routing/1/calculateRoute/"
         f"{req.lat},{req.long}:{location[req.dest][0]},{location[req.dest][1]}/json"
-        f"?traffic=true&travelMode=pedestrian&key={API_KEY}"
+        f"?traffic=true&travelMode=car&key={API_KEY}"
     )
-
-    print("REQUEST URL:", api_req)
 
     response = requests.get(api_req)
 
@@ -59,12 +78,16 @@ async def estimate_route(req: routing):
 
     data = response.json()
     result = data["routes"][0]["summary"]
+    total_sec = result["travelTimeInSeconds"]
+    hrs = total_sec // 3600
+    mins = (total_sec % 3600) // 60
+    secs = total_sec % 60
 
     return {
         "distance": result["lengthInMeters"]*0.0006213712,
-        "travel_time_hr": result["travelTimeInSeconds"] / 3600,
-        "travel_time_minutes": (result["travelTimeInSeconds"] % 3600) / 60,
-        "travel_time_sec": (result["travelTimeInSeconds"] % 3600) % 60,
+        "travel_time_hr": hrs,
+        "travel_time_minutes": mins,
+        "travel_time_sec": secs,
         "route": data
     }
 
